ed6d54fb98455cf6a69219a7f301f29ed63d9caf
Jose Martinez <jmmrcp@gmail.com>
2018-04-12 20:24:19 +0200

    Quiz 3

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var favicon = require('serve-favicon');
+
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -18,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quickControler = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quickControler.question);
+router.get('/quizes/answer', quickControler.answer);
 
 module.exports = router;
