acd0791f5debd5e59046ee2f28071c41396b79fb
Jose Martinez <jmmrcp@gmail.com>
2018-04-14 11:26:44 +0200

    toUpper

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,7 +10,7 @@ exports.question = function (req, res) {
 
 exports.answer = function (req, res) {
   models.Quiz.findAll().then(function (quiz) {
-    if (req.query.respuesta === quiz[0].respuesta) {
+    if (req.query.respuesta.toUpperCase() === quiz[0].respuesta.toUpperCase()) {
       res.render('quizes/answer', {
         respuesta: 'Correcto'
       });
